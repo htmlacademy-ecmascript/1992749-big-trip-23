7ae8a55d357a3b8172deef10e16f8324a65451c8
LevDiakov <381208dl@gmail.com>
2024-04-25 22:52:11 -0300

    Отрисует фильтры.

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -1,27 +1,18 @@
 import { createElement } from '../render';
+import { capitalizeLetter } from '../utils';
+
+const valuesToFilter = ['everything', 'future', 'present', 'past'];
 
 function createFilterTemplate() {
   return `
     <form class="trip-filters" action="#" method="get">
-      <div class="trip-filters__filter">
-        <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
-        <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-      </div>
 
+    ${valuesToFilter.map((item) => `
       <div class="trip-filters__filter">
-        <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future" disabled>
-        <label class="trip-filters__filter-label" for="filter-future">Future</label>
+        <input id="filter-${item}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${item}" checked>
+        <label class="trip-filters__filter-label" for="filter-${item}">${capitalizeLetter(item)}</label>
       </div>
-
-      <div class="trip-filters__filter">
-        <input id="filter-present" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="present">
-        <label class="trip-filters__filter-label" for="filter-present">Present</label>
-      </div>
-
-      <div class="trip-filters__filter">
-        <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past" disabled>
-        <label class="trip-filters__filter-label" for="filter-past">Past</label>
-      </div>
+      `).join('')}
 
       <button class="visually-hidden" type="submit">Accept filter</button>
     </form>
