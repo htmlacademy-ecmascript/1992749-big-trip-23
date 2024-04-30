86cd441073a7086dd3bafb531585f180827e3861
LevDiakov <381208dl@gmail.com>
2024-04-30 19:03:05 -0300

    Добавит в приложение Model.

---
+++ src/model/points-model.js
@@ -0,0 +1,9 @@
+import { points } from '../mock/points.js';
+
+export default class PointsModel {
+  points = points;
+
+  getPoints() {
+    return this.points;
+  }
+}
--- src/presenter/prime-presenter.js
+++ src/presenter/prime-presenter.js
@@ -8,20 +8,22 @@ import PointView from '../view/point-view/point-view';
 export default class PrimePresenter {
   listPointsContainer = new ListPointsView();
 
-  constructor() {
+  constructor({pointsModel}) {
     this.filtersContainer = document.querySelector('.trip-controls__filters');
     this.tripEventsContainer = document.querySelector('.trip-events');
 
+    this.pointsModel = pointsModel;
   }
 
   init() {
+    this.primePoints = [...this.pointsModel.getPoints()];
 
     render(new FilterView(), this.filtersContainer);
     render(new SortView(), this.tripEventsContainer);
     render(this.listPointsContainer, this.tripEventsContainer);
     render(new EditPointView(), this.listPointsContainer.getElement());
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < this.primePoints.length; i++) {
       render(new PointView(), this.listPointsContainer.getElement());
     }
 
