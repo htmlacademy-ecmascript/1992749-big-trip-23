2e33b358fb8908219f6bbe2a4ab56d36eed115ff
LevDiakov <381208dl@gmail.com>
2024-05-08 22:03:30 -0300

    Выделит показ точек маршрута в функцию

--- src/presenter/prime-presenter.js
+++ src/presenter/prime-presenter.js
@@ -2,7 +2,7 @@ import { render } from '../framework/render';
 import FilterView from '../view/filter-view/filter-view';
 import SortView from '../view/sort-view/sort-view';
 import ListPointsView from '../view/list-points-view/list-points-view';
-import EditPointView from '../view/edit-point-view/edit-point-view';
+//import EditPointView from '../view/edit-point-view/edit-point-view';
 import PointView from '../view/point-view/point-view';
 
 export default class PrimePresenter {
@@ -31,15 +31,16 @@ export default class PrimePresenter {
     render(new FilterView(), this.#filtersContainer);
     render(new SortView(), this.#tripEventsContainer);
     render(this.#listPointsContainer, this.#tripEventsContainer);
-    render(new EditPointView(
-      {point: this.#primePoints[10], destinations: this.#primeDestinations, offers: this.#primeOffers}
-    ), this.#listPointsContainer.element);
 
-    for (let i = 1; i < this.#primePoints.length; i++) {
-      render(new PointView(
-        {point: this.#primePoints[i], destinations: this.#primeDestinations, offers: this.#primeOffers}
-      ), this.#listPointsContainer.element);
+    for (let i = 0; i < this.#primePoints.length; i++) {
+      this.#renderPoint(this.#primePoints[i]);
     }
+  }
 
+  #renderPoint(point) {
+    const pointComponent = new PointView(
+      {point, destinations: this.#primeDestinations, offers: this.#primeOffers}
+    );
+    render(pointComponent, this.#listPointsContainer.element);
   }
 }
